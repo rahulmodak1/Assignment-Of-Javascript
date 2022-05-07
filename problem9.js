4ac47a6d07e2c4dd35db0e5803a0c485b90b786f
Rahul Modak <102527016+rahulmodak1@users.noreply.github.com>
2022-05-07 19:41:34 +0000

    update

--- problem9.js
+++ problem9.js
@@ -2,7 +2,7 @@
   function quad(a,b,c){
     let root1,root2;
     root1 = (-b + Math.sqrt(b*b - 4*a*c))/(2*a);
-    root2 = (b + Math.sqrt(b*b - 4*a*c))/(2*a);
+    root2 = (-b + Math.sqrt(b*b - 4*a*c))/(2*a);
     return [root1.toFixed(2),root2.toFixed(2)];
 }
  console.log(" Problem9 -" ,quad(1,5,6));  
